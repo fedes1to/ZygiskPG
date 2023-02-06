86125ff5325c21abe653348f5458b036f510f881
fedesito <lbertitoyt@gmail.com>
2023-02-06 16:42:40 +0100

    Revert "removed and64inlinehook"
    This reverts commit a5a675b8c88a8e0c20aa083249647a9656b1a14d.

--- module/src/main/cpp/hook.cpp
+++ module/src/main/cpp/hook.cpp
@@ -23,6 +23,9 @@
 #include "KittyMemory/KittyUtils.h"
 #include "Include/obfuscate.h"
 #include "Includes/Dobby/dobbyForHooks.h"
+#if defined(__aarch64__)
+#include "And64InlineHook/And64InlineHook.hpp"
+#endif
 
 using KittyMemory::ProcMap;
 using KittyScanner::RegisterNativeFn;
@@ -159,7 +162,9 @@ void *hack_thread(void *arg) {
     // example of a hex patch
     gPatches.maxLevel = MemoryPatch::createWithHex(g_il2cppBaseMap, 0x1C26554,"A0F08FD2C0035FD6");
     // example of a hook for arm64
-    DobbyHook((void*)(g_il2cppBaseMap.startAddress + 0x17139E8), (void*)WeaponSounds, (void**)&old_WeaponSounds);
+#if defined(__aarch64__)
+    A64HookFunction((void*)(g_il2cppBaseMap.startAddress + 0x17139E8), (void*)WeaponSounds, (void**)&old_WeaponSounds);
+#endif
     auto eglhandle = dlopen("libunity.so", RTLD_LAZY);
     auto eglSwapBuffers = dlsym(eglhandle, "eglSwapBuffers");
     DobbyHook((void*)eglSwapBuffers,(void*)hook_eglSwapBuffers,
