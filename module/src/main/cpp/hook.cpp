f7f33bc1d0093e6a05d2c916c282ca337641dd30
NitrogenDioxide <11productionsfelix@gmail.com>
2023-02-09 16:30:06 +0100

    i am gotte

--- module/src/main/cpp/hook.cpp
+++ module/src/main/cpp/hook.cpp
@@ -26,7 +26,17 @@
 
 #define GamePackageName "com.pixel.gun3d"
 
-
+/*
+ *
+ * LIST OF NOTES:
+ * GAMEMODES LIST: 丂丛万世丝七丒与丈, GameMode.cs << DONT TRUST THIS, ITS 丈下东与三丆下丛丄
+ * 丒丘丁丄丂丑丛丄东, ItemReward.cs (Scripts\\Store\\MainStore\\ItemReward.cs)
+ * 丒下丗丈丕三丌丏不, ProgressUpdater.Currency.cs (Scripts\\ServerProgressSync\\ProgressUpdater.Currency.cs)
+ * 丐三丐丄丝丅丈丄丞, OfferData.cs (Scripts/OfferPacks/OfferData.cs)
+ * SCENEINFO IS SceneInfo, FULLY OBFUSCATED
+ * SCENEINFO 且与丒与丈下丈丁一 = AddMode(GameMode)
+ *
+ */
 
 monoString* CreateIl2cppString(const char* str)
 {
@@ -51,7 +61,7 @@ gadgetUnlockApplied, isLoadScenePressed, modKeys, modKeysApplied, tgod, tgodappl
 removedrone, removedroneapplied, god, godapplied, ammo, ammoapplied, collectibles, collectiblesApplied, ezsuper, ezsuperApplied,
 crithit, crithitapplied, charm, weakness,fte,enemymarker, enableEditor, killboost, electric, kspeedboost, daterweapon, grenade,
 doublejump, catspam, coindrop, itemParams, blackMarket, blackMarketApplied, couponClicker, couponClickerApplied, setsClicker, setsClickerApplied,
-negativeCollectibles,anticheet,anticheetapplied, nullcollectibles, nullcollectiblesApplied, isDiscordPressed;
+negativeCollectibles,anticheet,anticheetapplied, nullcollectibles, nullcollectiblesApplied, isDiscordPressed, isKaxzWeps;
 
 float damage;
 
@@ -348,6 +358,24 @@ bool isDev(void *obj) {
     }
 }
 
+bool (*old_canBuy)(void *obj);
+bool canBuy(void *obj) {
+    if (isKaxzWeps)
+    {
+        return true;
+    }
+    return old_canBuy(obj);
+}
+
+bool (*old_isEvent)(void *obj);
+bool isEvent(void *obj) {
+    if (isKaxzWeps)
+    {
+        return false;
+    }
+    return old_isEvent(obj);
+}
+
 int isGame(JNIEnv *env, jstring appDataDir) {
     if (!appDataDir)
         return 0;
@@ -417,6 +445,8 @@ void DrawMenu(){
             ImGui::Checkbox("Force Critical Hits", &crithit);
             ImGui::Text("Forces Critical Shots each time you hit someone.");
             ImGui::Checkbox("Unlimited Ammo", &ammo);
+            ImGui::Checkbox("Edit Weapon Info", &isKaxzWeps);
+            ImGui::Text("Edits weapon info for every weapon");
         }
         if (ImGui::CollapsingHeader("Effects Mods")) {
             ImGui::Text("Add effects which ruin your enemys stats.");
@@ -541,6 +571,9 @@ void Modifications(){
     DobbyHook((void*)(g_il2cppBaseMap.startAddress + 0x17139E8), (void*)WeaponSounds, (void**)&oldWeaponSounds);
     DobbyHook((void*)(g_il2cppBaseMap.startAddress + 0x438120C), (void*)isEditor, (void**)&old_isEditor);
     DobbyHook((void*)(g_il2cppBaseMap.startAddress + 0x2ADECFC), (void*)isDev, (void**)&old_isDev);
+
+    DobbyHook((void*)(g_il2cppBaseMap.startAddress + 0x48F182C), (void*)canBuy, (void**)&old_canBuy);
+    DobbyHook((void*)(g_il2cppBaseMap.startAddress + 0x48F0500), (void*)isEvent, (void**)&old_isEvent);
 }
 
 void *hack_thread(void *arg) {
