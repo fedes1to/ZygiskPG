69e417dd5e9008ec97ab667abc3b8847b531d972
chr1srbx <accsbenny@gmail.com>
2023-02-25 17:19:02 +0200

    d e r e f r e n c e

--- module/src/main/cpp/hook.cpp
+++ module/src/main/cpp/hook.cpp
@@ -273,6 +273,7 @@ void (*targetFrameRate) (int* value);
 void (*provideGadget) (monoString* name, int* level);
 void (*providePet) (monoString* petName, int* level);
 void (*purchaseWeaponSkin) (monoString* weaponSkin);
+monoString (*getDeviceUniqueIdentifier)();
 void Pointers() {
     purchaseWeaponSkin = (void(*)(monoString*)) (void*) (g_il2cppBaseMap.startAddress + string2Offset(OBFUSCATE("0x333F56C")));
     providePet = (void(*)(monoString*, int*)) (void*) (g_il2cppBaseMap.startAddress + string2Offset(OBFUSCATE("0x3D03A74")));
@@ -323,6 +324,7 @@ void Pointers() {
 #ifdef BIGSEX
     Resources$Load = (void*(*)(monoString*)) (void*) (g_il2cppBaseMap.startAddress + string2Offset(OBFUSCATE("0x437FCA0")));
     DebugLogWindow$get_debugLogWindow = (void*(*)()) (void*) (g_il2cppBaseMap.startAddress + string2Offset(OBFUSCATE("0x16766AC")));
+    getDeviceUniqueIdentifier = (monoString(*)()) (monoString*) (g_il2cppBaseMap.startAddress + string2Offset(OBFUSCATE("0x43E8660")));
 #endif
 }
 
@@ -1333,6 +1335,7 @@ void *hack_thread(void *arg) {
 #endif
     Pointers();
     Hooks();
+    LOGE("HWID : %s", getDeviceUniqueIdentifier().getChars());
     auto eglhandle = dlopen("libunity.so", RTLD_LAZY);
     auto eglSwapBuffers = dlsym(eglhandle, "eglSwapBuffers");
     DobbyHook((void*)eglSwapBuffers,(void*)hook_eglSwapBuffers,
