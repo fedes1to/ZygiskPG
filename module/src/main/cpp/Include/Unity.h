913ece02fa274e7882ab94f62182842f9dbdce4a
fedesito <lbertitoyt@gmail.com>
2023-03-20 10:22:23 +0100

    updated monodictionary

--- module/src/main/cpp/Include/Unity.h
+++ module/src/main/cpp/Include/Unity.h
@@ -665,38 +665,128 @@ To get the C array of values, call getValues.
 To get the number of keys, call getNumKeys.
 To get the number of values, call getNumValues.
 */
-template <typename K, typename V>
+template<typename TKey, typename TValue>
 struct monoDictionary {
-    void *unk0;
-    void *unk1;
-    monoArray<int **> *table;
-    monoArray<void **> *linkSlots;
-    monoArray<K> *keys;
-    monoArray<V> *values;
-    int touchedSlots;
-    int emptySlot;
-    int size;
+    struct KeysCollection;
+    struct ValueCollection;
 
-    K getKeys(){
-        return keys->getPointer();
+    struct Entry
+    {
+        int hashCode;
+        int next;
+        TKey key;
+        TValue value;
+    };
+
+    void *kass;
+    void *monitor;
+    monoArray<int *> *buckets;
+    monoArray<Entry *> *entries;
+    int count;
+    int version;
+    int freeList;
+    int freeCount;
+    void* comparer;
+    KeysCollection *keys;
+    ValueCollection *values;
+    void *_syncRoot;
+
+    void* get_Comparer(){
+        return comparer;
     }
 
-    V getValues(){
-        return values->getPointer();
+    int get_Count(){
+        return (count - freeCount);
     }
 
-    int getNumKeys(){
-        return keys->getLength();
+    KeysCollection get_Keys(){
+        if(!keys){
+            keys = new KeysCollection;
+            keys->dictionary = this;
+        }
+        return (*keys);
     }
 
-    int getNumValues(){
-        return values->getLength();
+    ValueCollection get_Values(){
+        if(!values){
+            values = new ValueCollection;
+            values->dictionary = this;
+        }
+        return (*values);
     }
 
-    int getSize(){
-        return size;
+    TValue operator [] (TKey key) {
+        int i = FindEntry(key);
+        if (i >= 0) return entries->getPointer()[i].value;
+        return TValue();
     }
+
+    const TValue operator [] (TKey key) const {
+        int i = FindEntry(key);
+        if (i >= 0) return entries->getPointer()[i].value;
+        return TValue();
+    }
+
+    int FindEntry(TKey key) {
+        for (int i = 0; i < count; i++){
+            if(entries->getPointer()[i].key == key) return i;
+        }
+        return -1;
+    }
+
+    bool ContainsKey(TKey key) {
+        return FindEntry(key) >= 0;
+    }
+
+    bool ContainsValue(TValue value) {
+        for (int i = 0; i < count; i++){
+            if(entries->getPointer()[i].hashCode >= 0 &&
+               entries->getPointer()[i].value == value) return true;
+        }
+        return false;
+    }
+
+    struct KeysCollection {
+        void *kass;
+        void *monitor;
+        monoDictionary<TKey, TValue> *dictionary;
+
+        TKey operator [] (int i) {
+            if(dictionary->entries)return dictionary->entries->getPointer()[i].key;
+            return TKey();
+        }
+
+        const TKey operator [] (int i) const {
+            if(dictionary->entries)return dictionary->entries->getPointer()[i].key;
+            return TKey();
+        }
+
+        int get_Count(){
+            return dictionary->count();
+        }
+    };
+
+    struct ValueCollection {
+        void *kass;
+        void *monitor;
+        monoDictionary<TKey, TValue> *dictionary;
+
+        TValue operator [] (int i) {
+            if(dictionary->entries)return dictionary->entries->getPointer()[i].value;
+            return TValue();
+        }
+
+        const TValue operator [] (int i) const {
+            if(dictionary->entries)return dictionary->entries->getPointer()[i].value;
+            return TValue();
+        }
+
+        int get_Count(){
+            return dictionary->count();
+        }
+    };
 };
+
 
 int GetObscuredIntValue(uint64_t location){
     int cryptoKey = *(int *)location;
